305bedb84a19e85b52e2dc6537ffd7abe7a0e4ad
Your Name <you@example.com>
2019-07-28 11:36:04 +0900

    initialize repository

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/takashi/PycharmProjects/Django-Simple-CRUD-Sample-master/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
